d48e15e4b1daf2847539c7ceab536b01257ce2c3
Oliver O'Halloran <oohall@gmail.com>
2017-02-20 17:10:44 +1100

    HACK: dax: add dax device size IOCTL
    Usually you need to trawl sysfs and I'm lazy.
    
    Not-signed-off-by: Oliver O'Halloran <oohall@gmail.com>

--- drivers/dax/dax.c
+++ drivers/dax/dax.c
@@ -287,17 +287,21 @@ static struct dax_dev *to_dax_dev(struct device *dev)
 	return container_of(dev, struct dax_dev, dev);
 }
 
-static ssize_t size_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
+static unsigned long long dax_dev_size(struct dax_dev *dax_dev)
 {
-	struct dax_dev *dax_dev = to_dax_dev(dev);
 	unsigned long long size = 0;
 	int i;
 
 	for (i = 0; i < dax_dev->num_resources; i++)
 		size += resource_size(&dax_dev->res[i]);
 
-	return sprintf(buf, "%llu\n", size);
+	return size;
+}
+
+static ssize_t size_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%llu\n", dax_dev_size(to_dax_dev(dev)));
 }
 static DEVICE_ATTR_RO(size);
 
@@ -642,6 +646,21 @@ static int dax_release(struct inode *inode, struct file *filp)
 	return 0;
 }
 
+static long dax_ioctl(struct file *f, unsigned int cmd, unsigned long arg)
+{
+	void __user *output = (void __user *)arg;
+	unsigned long size;
+
+	if (cmd != IOC_DAX_SIZE)
+		return -EINVAL;
+
+	size = dax_dev_size(f->private_data);
+	if (copy_to_user(output, &size, sizeof(size)))
+		return -EFAULT;
+
+	return 0;
+}
+
 static const struct file_operations dax_fops = {
 	.llseek = noop_llseek,
 	.owner = THIS_MODULE,
@@ -649,6 +668,7 @@ static const struct file_operations dax_fops = {
 	.release = dax_release,
 	.get_unmapped_area = dax_get_unmapped_area,
 	.mmap = dax_mmap,
+	.unlocked_ioctl = dax_ioctl,
 };
 
 static void dax_dev_release(struct device *dev)
