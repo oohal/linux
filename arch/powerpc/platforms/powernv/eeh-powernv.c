427b597f210d09e6fe381baa4fa0c171467a32f9
Oliver O'Halloran <oohall@gmail.com>
2019-09-16 15:46:17 +1000

    powernv/eeh: Rework finding an existing edev in probe_pdev()
    Use the pnv_eeh_find_edev() helper to look up the eeh_dev for a device
    rather than doing it via the pci_dn.
    
    Signed-off-by: Oliver O'Halloran <oohall@gmail.com>

--- arch/powerpc/platforms/powernv/eeh-powernv.c
+++ arch/powerpc/platforms/powernv/eeh-powernv.c
@@ -394,14 +394,40 @@ static struct eeh_dev *pnv_eeh_probe(struct pci_dev *pdev)
 	int ret;
 	int bdfn = (pdev->bus->number << 8) | (pdev->devfn);
 
+	pci_dbg(pdev, "%s: probing\n", __func__);
+
 	/*
-	 * When probing the root bridge, which doesn't have any
-	 * subordinate PCI devices. We don't have OF node for
-	 * the root bridge. So it's not reasonable to continue
-	 * the probing.
+	 * EEH keeps the eeh_dev alive over a recovery pass even when the
+	 * corresponding pci_dev has been torn down. In that case we need
+	 * to find the existing eeh_dev and re-bind the two.
 	 */
-	if (!edev || edev->pe)
-		return NULL;
+	edev = pnv_eeh_find_edev(phb, bdfn);
+	if (edev) {
+		eeh_edev_dbg(edev, "Found existing edev!\n");
+
+		/*
+		 * XXX: eeh_remove_device() clears pdev so we shouldn't hit this
+		 * normally. I've found that screwing around with the pci probe
+		 * path can result in eeh_probe_pdev() being called twice. This
+		 * is harmless at the moment, but it's pretty strange so emit a
+		 * warning to be on the safe side.
+		 */
+		if (WARN_ON(edev->pdev))
+			eeh_edev_dbg(edev, "%s: already bound to a pdev!\n", __func__);
+
+		edev->pdev = pdev;
+
+		/* should we be doing something with REMOVED too? */
+		edev->mode &= EEH_DEV_DISCONNECTED;
+
+		/* update the primary bus if we need to */
+		// XXX: why do we need to do this? is the pci_bus going away? what cleared the flag?
+		if (!(edev->pe->state & EEH_PE_PRI_BUS)) {
+			edev->pe->bus = pdev->bus;
+			if (edev->pe->bus)
+				edev->pe->state |= EEH_PE_PRI_BUS;
+		}
+	}
 
 	/* already configured? */
 	if (edev->pdev) {
@@ -414,8 +440,6 @@ static struct eeh_dev *pnv_eeh_probe(struct pci_dev *pdev)
 	/* Skip for PCI-ISA bridge */
 	if ((pdev->class >> 8) == PCI_CLASS_BRIDGE_ISA)
 		return NULL;
-
-	eeh_edev_dbg(edev, "Probing device\n");
 
 	/* Initialize eeh device */
 	edev->mode	&= 0xFFFFFF00;
