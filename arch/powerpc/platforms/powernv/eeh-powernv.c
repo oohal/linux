e87758e0e17e812e1262fee7925307ee39d691de
Oliver O'Halloran <oohall@gmail.com>
2019-09-16 16:19:59 +1000

    powernv/eeh: Allocate eeh_dev's when needed
    Have the PowerNV EEH backend allocate the eeh_dev if needed rather than using
    the one attached to the pci_dn. This gets us most of the way towards decoupling
    pci_dn from the PowerNV EEH code.
    
    Signed-off-by: Oliver O'Halloran <oohall@gmail.com>
    ---
    We should probably be free()ing the eeh_dev somewhere. The pci_dev release
    function is the right place for it.

--- arch/powerpc/platforms/powernv/eeh-powernv.c
+++ arch/powerpc/platforms/powernv/eeh-powernv.c
@@ -385,11 +385,11 @@ static struct eeh_pe *pnv_eeh_get_upstream_pe(struct pci_dev *pdev)
  */
 static struct eeh_dev *pnv_eeh_probe(struct pci_dev *pdev)
 {
-	struct pci_dn *pdn = pci_get_pdn(pdev);
-	struct pci_controller *hose = pdn->phb;
+	struct pnv_phb *phb = pci_bus_to_pnvhb(pdev->bus);
+	struct pci_controller *hose = phb->hose;
 	struct pnv_phb *phb = hose->private_data;
-	struct eeh_dev *edev = pdn_to_eeh_dev(pdn);
 	struct eeh_pe *upstream_pe;
+	struct eeh_dev *edev;
 	uint32_t pcie_flags;
 	int ret;
 	int bdfn = (pdev->bus->number << 8) | (pdev->devfn);
@@ -441,12 +441,27 @@ static struct eeh_dev *pnv_eeh_probe(struct pci_dev *pdev)
 	if ((pdev->class >> 8) == PCI_CLASS_BRIDGE_ISA)
 		return NULL;
 
+	/* otherwise allocate and initialise a new eeh_dev */
+	edev = kzalloc(sizeof(*edev), GFP_KERNEL);
+	if (!edev) {
+		pr_err("%s: out of memory lol\n", __func__);
+		return NULL;
+	}
+
 	/* Initialize eeh device */
 	edev->mode	&= 0xFFFFFF00;
+	edev->bdfn       = bdfn;
+	edev->controller = phb->hose;
+
 	edev->pcix_cap = pci_find_capability(pdev, PCI_CAP_ID_PCIX);
 	edev->pcie_cap = pci_find_capability(pdev, PCI_CAP_ID_EXP);
 	edev->af_cap   = pci_find_capability(pdev, PCI_CAP_ID_AF);
 	edev->aer_cap  = pci_find_ext_capability(pdev, PCI_EXT_CAP_ID_ERR);
+
+	/* TODO: stash the vf_index in here? */
+	if (pdev->is_virtfn)
+		edev->physfn = pdev->physfn;
+
 	if ((pdev->class >> 8) == PCI_CLASS_BRIDGE_PCI) {
 		edev->mode |= EEH_DEV_BRIDGE;
 		if (edev->pcie_cap) {
